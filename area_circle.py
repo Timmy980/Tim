aef8162fcaa327b1d8b01d12984f573342fd882c
Timmy980 <62102011+Timmy980@users.noreply.github.com>
2020-03-24 18:16:12 +0100

    Add files via upload

---
+++ area_circle.py
@@ -0,0 +1,9 @@
+# This program accepts the radius of a circle
+# and calculates the area
+from math import pi
+
+radius = int(input('Input the radius of the circle:  '))
+
+area = pi * radius**2
+
+print('The area is: ', area)
